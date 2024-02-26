71bbbe3849750ec3e76de01f3332cb3b877d85e8
AbdulrahmanIsmailMohamed <abdulrahman.ismail.mohammed@gmail.com>
2024-02-27 00:54:52 +0200

    add raster

--- frontend/src/components/Map.tsx
+++ frontend/src/components/Map.tsx
@@ -1,7 +1,27 @@
-import { MapContainer, Marker, Popup, TileLayer } from "react-leaflet";
+import {
+  ImageOverlay,
+  LayerGroup,
+  LayersControl,
+  MapContainer,
+  Marker,
+  Polygon,
+  Popup,
+  TileLayer,
+} from "react-leaflet";
 import zag from "../data/zag.json";
 
 export const Map = () => {
+  const pathRaster = "zag.jpg";
+  const polygon: any = [
+    [30.601313, 31.524711],
+    [30.578113, 31.549962],
+    [30.572792, 31.549612],
+    [30.562889, 31.504607],
+    [30.58166, 31.466473],
+    [30.601313, 31.468878],
+    [30.610473, 31.489669],
+  ];
+
   return (
     <MapContainer
       center={[30.587233051962436, 31.5202796459198]}
@@ -12,28 +32,40 @@ export const Map = () => {
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
       />
-      {zag.features.map((item) => (
-        <Marker
-          key={item.id}
-          position={[
-            item.geometry.coordinates[1]!,
-            item.geometry.coordinates[0]!,
-          ]}
-        >
-          <Popup>
-            <div>
-              <h3>{item.properties.name}</h3>
-              <p>{item.properties.type}</p>
-              <img
-                src={item.properties.photo.replace("\\", "/")}
-                alt="this point not contain an image"
-                height="150px"
-                width="250px"
-              />
-            </div>
-          </Popup>
-        </Marker>
-      ))}
+      <LayersControl position="topright">
+        <LayersControl.Overlay name="Emergency Managment with popup">
+          <LayerGroup>
+            {zag.features.map((item) => (
+              <Marker
+                key={item.id}
+                position={[
+                  item.geometry.coordinates[1]!,
+                  item.geometry.coordinates[0]!,
+                ]}
+              >
+                <Popup>
+                  <div>
+                    <h3>{item.properties.name}</h3>
+                    <p>{item.properties.type}</p>
+                    <img
+                      src={item.properties.photo.replace("\\", "/")}
+                      alt="this point not contain an image"
+                      height="150px"
+                      width="250px"
+                    />
+                  </div>
+                </Popup>
+              </Marker>
+            ))}
+          </LayerGroup>
+        </LayersControl.Overlay>
+        <LayersControl.Overlay name="Industry">
+        <Polygon pathOptions={{ color: 'red', fillColor: 'red' }} positions={polygon} />
+        </LayersControl.Overlay>
+        <LayersControl.Overlay name="raster">
+          <ImageOverlay bounds={polygon} url={pathRaster} />
+        </LayersControl.Overlay>
+      </LayersControl>
     </MapContainer>
   );
 };
